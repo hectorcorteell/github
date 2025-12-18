1b41991b5f67bb2eb83eff5045823fd8824cd0e6
hector <heccormir2@alu.edu.gva.es>
2025-12-18 13:14:54 +0100

    Modificado el último print de Despedida.java

--- src/Despedida.java
+++ src/Despedida.java
@@ -5,6 +5,6 @@ public class Despedida {
         Scanner sc = new Scanner(System.in);
         System.out.println("Dime tu nombre");
         String nombre = sc.nextLine();
-        System.out.println("Adiós"+nombre);
+        System.out.println("Adiós"+nombre+". Espero verte más a menudo.");
     }
 }
