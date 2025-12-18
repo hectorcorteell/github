1617fa9c9f45366e355ef79c40416185dc310c3d
hector <heccormir2@alu.edu.gva.es>
2025-12-18 12:29:42 +0100

    Clase Saludo: añadido saludo

--- src/Saludo.java
+++ src/Saludo.java
@@ -1,3 +1,12 @@
-public class Saludo {
+import java.util.Scanner;
 
+public class Saludo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime tu nombre");
+        String nombre = sc.nextLine();
+
+        System.out.println("Hola"+nombre);
+        sc.close();
+    }
 }
